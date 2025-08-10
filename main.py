f8d757f7a7a51e6b23a7e3d84ae6cef1d63e80b8
albinjoby <albinjobyponnu@gmail.com>
2025-08-10 12:02:07 +0530

    add web scrapping on billboard:

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ date = input("enter the data in this format YYYY-MM-DD: ")
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 url = "https://www.billboard.com/charts/hot-100/" + date
 response = requests.get(url=url, headers=header)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_span = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_span]
+
